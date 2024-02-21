ad1c22c69b2c3af1f352e9fa66c33e3065987466
Reahana <reahana233@gmail.com>
2024-02-21 19:23:14 +0600

    area

--- app/Http/Controllers/AreaController.php
+++ app/Http/Controllers/AreaController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.area.add');
+    }
 }
--- resources/views/admin/area/add.blade.php
+++ resources/views/admin/area/add.blade.php
@@ -1,7 +1,39 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Prodiv
- * Date: 2/21/2024
- * Time: 12:55 PM
- */
+@extends('admin.master')
+
+@section('body')
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="header-title">Add Area form</h4>
+                    <p class="text-muted font-14">{{Session::get('message')}}</p>
+                    <form class="form-horizontal" action="" method="POST" enctype="multipart/form-data">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="inputEmail31" class="col-3 col-form-label">Area no</label>
+                            <div class="col-9">
+                                <input type="number" class="form-control" name="area_no" id="area_no" placeholder="Area no"/>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label for="inputEmail3" class="col-3 col-form-label"> Area Name</label>
+                            <div class="col-9">
+                                <input type="text" class="form-control" name="name" id="name" placeholder=" Area Name"/>
+                            </div>
+                        </div>
+                       
+
+
+                        <div class="justify-content-end row">
+                            <div class="col-9">
+                                <button type="submit" class="btn btn-info">Add New NID</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>  <!-- end card-body -->
+            </div>  <!-- end card -->
+        </div>  <!-- end col -->
+    </div>
+@endsection
