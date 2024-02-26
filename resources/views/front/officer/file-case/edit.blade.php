b2570ebc2c4445bd7ebf368e508aa4cd54948e2f
Reahana <reahana233@gmail.com>
2024-02-26 23:39:27 +0600

    edit case

--- resources/views/front/officer/file-case/edit.blade.php
+++ resources/views/front/officer/file-case/edit.blade.php
@@ -1,13 +1,13 @@
 @extends('front.officer.master')
 @section('title')
-    Add new Case |
+    Edit new Case |
 @endsection
 @section('body')
     <div class="row">
         <div class="col-md-12">
             <div class="ibox mt-3">
                 <div class="ibox-head">
-                    <div class="ibox-title">Add Case Form</div>
+                    <div class="ibox-title">Edit Case Form</div>
                     <div class="ibox-tools">
                         <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
                         <a class="fullscreen-link"><i class="fa fa-expand"></i></a>
@@ -22,12 +22,12 @@
                             </button>
                         </div>
                     @endif
-                    <form class="form-horizontal" action="{{route('case.new')}}" method="POST" enctype="multipart/form-data">
+                    <form class="form-horizontal" action="{{route('case.update', ['id' => $case->id])}}" method="POST" enctype="multipart/form-data">
                         @csrf
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label"> Case ID</label>
                             <div class="col-sm-10">
-                                <input class="form-control"  type="number" name="c_id" placeholder=" Case ID"/>
+                                <input class="form-control"  type="number" value="{{$case->c_id}}" name="c_id" placeholder=" Case ID"/>
 
                             </div>
                         </div>
@@ -37,7 +37,7 @@
                                 <select class="form-control" name="nid" id="nid">
                                     <option> -- Select NID -- </option>
                                     @foreach($citizens as $citizen)
-                                        <option value="{{$citizen->nid}}">{{$citizen->nid}}</option>
+                                        <option value="{{$citizen->nid}}" {{$citizen->nid == $case->nid ? 'selected' : ''}}>{{$citizen->nid}}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -48,7 +48,7 @@
                                 <select class="form-control" name="name" id="name">
                                     <option> --  Name -- </option>
                                     @foreach($citizens as $citizen)
-                                        <option value="{{$citizen->name}}">{{$citizen->name}}</option>
+                                        <option value="{{$citizen->name}}" {{$citizen->name == $case->name ? 'selected' : ''}}>{{$citizen->name}}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -58,7 +58,7 @@
                         <div class="form-group row">
                             <label class="col-sm-2 col-form-label">Contact Number</label>
                             <div class="col-sm-10">
-                                <input class="form-control" type="number" name="contact" placeholder="Contact Number"/>
+                                <input class="form-control" type="number" value="{{$case->contact}}" name="contact" placeholder="Contact Number"/>
                             </div>
                         </div>
 
@@ -68,7 +68,7 @@
                                 <select class="form-control" name="clause" id="clause">
                                     <option> -- Select Clause -- </option>
                                     @foreach($laws as $law)
-                                        <option value="{{$law->clause}}">{{$law->clause}}</option>
+                                        <option value="{{$law->clause}} " {{$law->clause == $case->clause ? 'selected' : ''}}>{{$law->clause}}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -79,7 +79,7 @@
                                 <select class="form-control" name="title" id="title">
                                     <option> --Law -- </option>
                                     @foreach($laws as $law)
-                                        <option value="{{$law->title}}">{{$law->title}}</option>
+                                        <option value="{{$law->title}}" {{$law->title == $case->title ? 'selected' : ''}}>{{$law->title}}</option>
                                     @endforeach
                                 </select>
 
@@ -88,7 +88,7 @@
 
                         <div class="form-group row">
                             <div class="col-sm-10 ml-sm-auto">
-                                <button class="btn btn-info btn-block" type="submit">File New Case</button>
+                                <button class="btn btn-info btn-block" type="submit">Edit Case</button>
                             </div>
                         </div>
                     </form>
