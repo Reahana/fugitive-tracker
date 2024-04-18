52934de446042eebd2d90110029b6216c0e2d6ff
Reahana <reahana233@gmail.com>
2024-04-18 22:58:49 +0600

    footer

--- resources/views/front/officer/includes/footer.blade.php
+++ resources/views/front/officer/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="page-footer">
     <div class="float-start font-13"><script>document.write(new Date().getFullYear())</script> © <b>Fugitive Tracker</b> - All rights reserved.</div>
     <div class="float-end ">
-        Design & Developed By<a class="px-4" href="https://reahanagomes.com/" style="color: crimson" target="_blank">Amanada Reahana Gomes</a>
+        Design & Developed By<a class="px-4" href="https://reahana.github.io/AmandaReahanaGomes.Portfolio/" style="color: crimson" target="_blank">Amanada Reahana Gomes</a>
     </div>
 
     <div class="to-top"><i class="fa fa-angle-double-up"></i></div>
